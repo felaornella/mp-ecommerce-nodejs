0f1688bec9273b557f4b2ad207c70a565b7f0e66
felaornella <64591475+felaornella@users.noreply.github.com>
2020-10-03 17:24:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,4 +18,7 @@ app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
  
-app.listen(3000);
+app.set('port', (process.env.PORT || 5000)); 
+app.listen(app.get('port'), function() { 
+    console.log('App is running, server is listening on port ', app.get('port')); 
+});
